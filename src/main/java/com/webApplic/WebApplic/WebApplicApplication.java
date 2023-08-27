16f3918e3ce855f4b9ecdce63284f3de42292868
BboyMogun <mohamed12rawda@gmail.com>
2023-08-27 17:45:57 +0200

    Initial commit

---
+++ src/main/java/com/webApplic/WebApplic/WebApplicApplication.java
@@ -0,0 +1,13 @@
+package com.webApplic.WebApplic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webApplic/WebApplic/WebApplicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webApplic.WebApplic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
